9e5155f8b0d25607fb950b540e976c2adecd3bf6
Aryan Pandit <a12022005@gmail.com>
2024-03-15 04:09:32 +0530

    buyersresponsive

--- src/app/buyer/page.tsx
+++ src/app/buyer/page.tsx
@@ -8,79 +8,76 @@ export function Loadmore() {
 
 const page = () => {
     return (
-        <div className="my-5 flex h-full w-full flex-row justify-center gap-5 px-36">
-            <div className="h-[100svh] w-[25svh] rounded-tl-lg bg-purple-400 text-center ">
+        <div className="my-5 flex h-full w-full flex-col lg:flex-row justify-center gap-5 px-12 lg:px-36">
+            <div className="h-[300px] w-full lg:h-[100svh] lg:w-[45svw] rounded-t-lg lg:rounded-tr-none bg-purple-400 text-center ">
                 Filters
             </div>
-            <div className="flex h-full w-[65svh] flex-col items-center gap-5 bg-purple-400 text-center">
+            <div className="flex h-full w-full flex-col items-center gap-5 bg-purple-400 text-center">
                 <span>property listing</span>
-                <div className="flex h-[30svh] max-w-[90%] items-center gap-2 rounded-lg bg-purple-500 p-4">
+                <div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500 p-4 ">
                     <img
                         src="/house.svg"
                         alt=""
-                        className="h-full w-[20svw] "
+                        className="h-full w-[50svw]"
                     />
                     <div className="flex h-full w-full flex-col gap-8">
-                        <div className="flex flex-col">
+                    <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
                             <span className="text-xs">view EMI options</span>
                         </div>
-                        <div className="flex flex-col items-end">
+                        <div className="flex flex-col lg:items-end">
                             <span className="text-xl">1080sq.ft</span>
                             <span>3 BHK</span>
                             <span>3.0k per sq.ft</span>
                         </div>
                     </div>
                 </div>
-                <div className="flex h-[30svh] max-w-[90%] items-center gap-2 rounded-lg bg-purple-500 p-4">
+                <div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500 p-4 ">
                     <img
                         src="/house.svg"
                         alt=""
-                        className="h-full w-[20svw] "
+                        className="h-full w-[50svw]"
                     />
                     <div className="flex h-full w-full flex-col gap-8">
-                        <div className="flex flex-col">
+                    <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
                             <span className="text-xs">view EMI options</span>
                         </div>
-                        <div className="flex flex-col items-end">
+                        <div className="flex flex-col lg:items-end">
                             <span className="text-xl">1080sq.ft</span>
                             <span>3 BHK</span>
                             <span>3.0k per sq.ft</span>
                         </div>
                     </div>
-                </div>
-                <div className="flex h-[30svh] max-w-[90%] items-center gap-2 rounded-lg bg-purple-500 p-4">
+                </div><div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500 p-4 ">
                     <img
                         src="/house.svg"
                         alt=""
-                        className="h-full w-[20svw] "
+                        className="h-full w-[50svw]"
                     />
                     <div className="flex h-full w-full flex-col gap-8">
-                        <div className="flex flex-col">
+                    <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
                             <span className="text-xs">view EMI options</span>
                         </div>
-                        <div className="flex flex-col items-end">
+                        <div className="flex flex-col lg:items-end">
                             <span className="text-xl">1080sq.ft</span>
                             <span>3 BHK</span>
                             <span>3.0k per sq.ft</span>
                         </div>
                     </div>
-                </div>
-
-                <div className="flex h-[30svh] max-w-[90%] items-center gap-2 rounded-lg bg-purple-500 p-4">
+                </div><div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500 p-4 ">
                     <img
                         src="/house.svg"
                         alt=""
-                        className="h-full w-[20svw] "
+                        className="h-full w-[50svw]"
                     />
                     <div className="flex h-full w-full flex-col gap-8">
-                        <div className="flex flex-col">
+                    <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
                             <span className="text-xs">view EMI options</span>
                         </div>
-                        <div className="flex flex-col items-end">
+                        <div className="flex flex-col lg:items-end">
                             <span className="text-xl">1080sq.ft</span>
                             <span>3 BHK</span>
                             <span>3.0k per sq.ft</span>
@@ -89,7 +86,7 @@ const page = () => {
                 </div>
                 <Button>Load More</Button>
             </div>
-            <div className="h-[40svh] w-[25svh] rounded-tr-lg bg-purple-400 text-center">
+            <div className="h-[40svh] w-[60svh] hidden lg:block rounded-tr-lg bg-purple-400 text-center">
                 More Info
             </div>
         </div>
