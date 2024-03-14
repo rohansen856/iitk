bcdcd8b4aa1e3318ce10d2707e2a90f9bcc8f0d1
Aryan Pandit <a12022005@gmail.com>
2024-03-14 20:47:02 +0530

    buyersmall

--- src/app/buyer/page.tsx
+++ src/app/buyer/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
     <div className='flex justify-center gap-5 flex-row px-36 my-5 h-full w-full'>
     <div className='bg-purple-400 h-[100svh] w-[25svh] rounded-tl-lg text-center '>Filters</div>
     <div className='bg-purple-400 h-[100svh] w-[65svh] text-center flex flex-col gap-5 items-center'><span>property listing</span>
-      <div className='w-[35svw] h-[35svh] bg-purple-600 rounded-lg flex items-center p-4'>
+      <div className='w-[35svw] h-[35svh] bg-purple-500 rounded-lg flex items-center p-4'>
         <img src="/house.svg" alt="" className='h-full w-[25svw] ' /> 
           <div className='flex flex-col items-start h-full mx-2'>
           <span className='text-xl'>$22L-44L</span>
