3c1643a14c5772d559a6c668414f191a82cc08a5
rcsen <rohansen856@gmail.com>
2024-03-14 21:43:23 +0530

    modified:   src/app/(marketing)/page.tsx

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,10 +1,4 @@
-import Image from "next/image"
-import Link from "next/link"
 
-import { absoluteUrl, cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { SideNav } from "@/components/side-nav"
-import { TypeWriter } from "@/components/type-writer"
 
 export default async function IndexPage() {
     return (
