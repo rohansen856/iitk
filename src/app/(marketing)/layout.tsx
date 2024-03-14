3e15f5a577c14409de7a55b530ae1b0a5c29779e
--unset <--unset>
2024-03-15 00:39:15 +0530

    stuff

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
+import Buildings from "@/components/Buildings"
 
 interface MarketingLayoutProps {
     children: React.ReactNode
@@ -18,6 +19,7 @@ export default async function MarketingLayout({
 
     return (
         <div className="flex min-h-screen flex-col">
+            <Buildings />
             <header className="container z-40 bg-background">
                 <div className="flex h-20 items-center justify-between py-6">
                     <MainNav items={marketingConfig.mainNav} />
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,46 +1,16 @@
-import Image from "next/image"
-import Link from "next/link"
-
-import { absoluteUrl, cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { SideNav } from "@/components/side-nav"
-import { TypeWriter } from "@/components/type-writer"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
 
 export default async function IndexPage() {
     return (
         <>
-            <div className="fixed z-[-20] flex h-[80vh] w-full">
-                <SideNav />
-                <div className="relative flex h-full w-full">
-                    <Image
-                        src="/mask.png"
-                        alt="hecker"
-                        className="m-auto opacity-10"
-                        width={450}
-                        height={450}
-                        priority
-                    />
+            <div className="flex h-[80vh] w-full justify-center items-center">
+                <div className="w-2/5 relative flex items-center justify-center">
+                <Input className="py-6 rounded-full bg-white/80 focus-visible:outline-purple-400 focus-visible:outline-2 " type="text" placeholder="Search" />
+                <Button variant={"ghost"} className="hover:bg-transparent absolute right-0 rounded-full hover:scale-125 transition-all duration-150" type="submit"><Search color="black" /></Button>
                 </div>
             </div>
-            <section className="flex min-h-screen w-full flex-col items-center overflow-hidden pl-0 pt-24 md:pl-[150px] md:pt-20">
-                <div className="flex flex-col items-center space-y-6">
-                    <h1 className="animate-gradient bg-clip-text text-center font-heading text-4xl text-transparent sm:text-5xl md:text-6xl xl:text-7xl">
-                        The &nbsp;Heckers&nbsp;of&nbsp;IIITDMJ
-                    </h1>
-                    <h2 className="h-16 max-w-[42rem] bg-clip-text text-2xl leading-normal text-muted-foreground sm:text-3xl sm:leading-8 md:text-3xl lg:text-3xl">
-                        <TypeWriter />
-                    </h2>
-                </div>
-                <Link
-                    href={absoluteUrl("/peoples")}
-                    className={cn(
-                        buttonVariants({ size: "lg" }),
-                        "mt-32 rounded bg-primary text-xl"
-                    )}
-                >
-                    Profile
-                </Link>
-            </section>
         </>
     )
 }
