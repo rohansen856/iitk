e2c207e0f237a3e3b124a7dc80fef72f1cd86f5a
rcsen <rohansen856@gmail.com>
2024-03-15 10:30:11 +0530

    modified:   src/app/(auth)/layout.tsx 	modified:   src/app/buyer/layout.tsx 	modified:   src/app/seller/layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,17 @@
 interface AuthLayoutProps {
-  children: React.ReactNode
+    children: React.ReactNode
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
-  return <div className="min-h-screen">{children}</div>
+    return (
+        <div className="min-h-screen relative">
+            <div className="absolute w-screen h-screen">
+                <div className="relative h-full w-full bg-slate-950">
+                    {/* eslint-disable-next-line tailwindcss/no-contradicting-classname */}
+                    <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:35px_35px]"></div>
+                </div>
+            </div>
+            {children}
+        </div>
+    )
 }
