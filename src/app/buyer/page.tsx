713412a381c2dabb72f07f2ae0d9cdce6d2e6243
rcsen <rohansen856@gmail.com>
2024-03-15 13:16:16 +0530

    modified:   src/app/buyer/page.tsx

--- src/app/buyer/page.tsx
+++ src/app/buyer/page.tsx
@@ -8,18 +8,14 @@ export function Loadmore() {
 
 const page = () => {
     return (
-        <div className="my-5 flex h-full w-full flex-col lg:flex-row justify-center gap-5 px-12 lg:px-36 ">
-            <div className="h-[300px] w-full lg:h-[100svh] lg:w-[45svw] rounded-t-lg lg:rounded-tr-none bg-purple-800/40 text-center py-3">
+        <div className="my-5 flex h-full w-full flex-col justify-center gap-5 px-12 lg:flex-row lg:px-36">
+            <div className="h-[300px] w-full rounded-t-lg bg-purple-400 text-center lg:h-[100svh] lg:w-[45svw] lg:rounded-tr-none ">
                 Filters
             </div>
-            <div className="flex h-full w-full flex-col items-center gap-5 bg-purple-800/40 text-center">
-                <span>Property listings</span>
-                <div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500/50 p-4 ">
-                    <img
-                        src="/house.svg"
-                        alt=""
-                        className="h-full w-[50svw]"
-                    />
+            <div className="flex h-full w-full flex-col items-center gap-5 bg-purple-400 text-center">
+                <span>property listing</span>
+                <div className="flex h-full max-w-[95%] flex-col items-start gap-2 rounded-lg bg-purple-500 p-4 lg:flex-row ">
+                    <img src="/house.svg" alt="" className="h-full w-[50svw]" />
                     <div className="flex h-full w-full flex-col gap-8">
                         <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
@@ -32,17 +28,8 @@ const page = () => {
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
                 <div className="flex h-full max-w-[95%] flex-col items-start gap-2 rounded-lg bg-purple-500 p-4 lg:flex-row ">
                     <img src="/house.svg" alt="" className="h-full w-[50svw]" />
-=======
-                <div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500/50 p-4 ">
-                    <img
-                        src="/house.svg"
-                        alt=""
-                        className="h-full w-[50svw]"
-                    />
->>>>>>> aacf034677f3a9e08fca78e4808bf1ea93eee6a2
                     <div className="flex h-full w-full flex-col gap-8">
                         <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
@@ -54,18 +41,9 @@ const page = () => {
                             <span>3.0k per sq.ft</span>
                         </div>
                     </div>
-<<<<<<< HEAD
                 </div>
                 <div className="flex h-full max-w-[95%] flex-col items-start gap-2 rounded-lg bg-purple-500 p-4 lg:flex-row ">
                     <img src="/house.svg" alt="" className="h-full w-[50svw]" />
-=======
-                </div><div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500/50 p-4 ">
-                    <img
-                        src="/house.svg"
-                        alt=""
-                        className="h-full w-[50svw]"
-                    />
->>>>>>> aacf034677f3a9e08fca78e4808bf1ea93eee6a2
                     <div className="flex h-full w-full flex-col gap-8">
                         <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
@@ -77,18 +55,9 @@ const page = () => {
                             <span>3.0k per sq.ft</span>
                         </div>
                     </div>
-<<<<<<< HEAD
                 </div>
                 <div className="flex h-full max-w-[95%] flex-col items-start gap-2 rounded-lg bg-purple-500 p-4 lg:flex-row ">
                     <img src="/house.svg" alt="" className="h-full w-[50svw]" />
-=======
-                </div><div className="flex flex-col lg:flex-row h-full max-w-[95%] items-start gap-2 rounded-lg bg-purple-500/50 p-4 ">
-                    <img
-                        src="/house.svg"
-                        alt=""
-                        className="h-full w-[50svw]"
-                    />
->>>>>>> aacf034677f3a9e08fca78e4808bf1ea93eee6a2
                     <div className="flex h-full w-full flex-col gap-8">
                         <div className="flex flex-col lg:items-end">
                             <span className="text-2xl">$22L-44L</span>
@@ -103,11 +72,7 @@ const page = () => {
                 </div>
                 <Button>Load More</Button>
             </div>
-<<<<<<< HEAD
             <div className="hidden h-[40svh] w-[60svh] rounded-tr-lg bg-purple-400 text-center lg:block">
-=======
-            <div className="h-[40svh] w-[60svh] hidden lg:block rounded-tr-lg bg-purple-800/40 text-center">
->>>>>>> aacf034677f3a9e08fca78e4808bf1ea93eee6a2
                 More Info
             </div>
         </div>
