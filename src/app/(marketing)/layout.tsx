e5510536ab1b5d1e9d9140c02cf20968a3f0614d
rcsen <rohansen856@gmail.com>
2024-03-15 09:58:41 +0530

    modified:   src/app/(marketing)/layout.tsx

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -22,7 +22,7 @@ export default async function MarketingLayout({
             <div className="absolute w-screen h-screen">
                 <div className="relative h-full w-full bg-slate-950">
                     {/* eslint-disable-next-line tailwindcss/no-contradicting-classname */}
-                    <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+                    <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:35px_35px]"></div>
                 </div>
             </div>
             <Buildings />
